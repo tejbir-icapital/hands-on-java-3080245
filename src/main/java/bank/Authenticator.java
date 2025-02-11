4b3e55c5791704ea2b2d9e26d4226755a2967f87
tejbir-icapital <tejbir.singh@icapitalnetwork.com>
2025-02-11 21:46:39 +0000

    add Menu.java. fix small bug in the Authenticator class.

--- src/main/java/bank/Authenticator.java
+++ src/main/java/bank/Authenticator.java
@@ -14,8 +14,7 @@ public class Authenticator {
       customer.setAuthenticated(true);
       return customer;
     } 
-    else throw new LoginException("Incorrect password.")
-
+    else throw new LoginException("Incorrect password.");
   }
 
   public static void logout(Customer customer) {

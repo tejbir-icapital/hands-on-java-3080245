6e4228fd4b629e7a9ddf9148198604f414e46590
tejbir-icapital <tejbir.singh@icapitalnetwork.com>
2025-02-11 17:42:54 +0000

    Add the Account and DataSource classes to handle the SQL connection.

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,39 @@
+package bank;
+
+public class Account {
+
+private int accountId;
+private String accountType;
+private double balance;
+
+public Account(int accountId, String accountType, double balance) {
+  setAccountId(accountId);
+  setAccountType(accountType);
+  setBalance(balance);
+}
+
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+  public String getAccountType() {
+    return this.accountType;
+  }
+
+  public void setAccountType(String accountType) {
+    this.accountType = accountType;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+}
---
+++ src/main/java/bank/Datasource.java
@@ -0,0 +1,24 @@
+package bank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Datasource {
+
+  public static Connection connect() {
+    String db_file = "jdbc:sqlite:resources/bank.db";
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection(db_file);
+      System.out.println("We're connected!");
+    }catch(SQLException e) {
+      e.printStackTrace();
+    }
+    return connection;
+  }
+
+  public static void main(String[] args) {
+    connect();
+  }
+}
